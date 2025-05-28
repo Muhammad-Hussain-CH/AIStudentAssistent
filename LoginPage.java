58876153db8447bb7aaac5a29e7e02a381353dc8
Muhammad Farhan <142079152+muhammadfarhan024260@users.noreply.github.com>
2025-05-28 11:56:24 +0500

    Update LoginPage.java
    Corrected validations and connecting ForgetPasswordPage and DashboardPage

--- LoginPage.java
+++ LoginPage.java
@@ -44,9 +44,23 @@ public class LoginPage {
     private RoundedButton signInButton;
 
     private String placeholder = "Enter your email or username";
-    private String placeholder2 = "Password";
+    private String placeholder2 = "PaSsWoRd";
 
-    public LoginPage() {
+    private int length = 420;
+
+    private boolean isEmailFieldSpaced = false;
+    private boolean isPassFieldSpaced = false;
+    private static boolean showForgetPasswordPage = false;
+    private static boolean showDashboardPage = true;
+    private boolean[] canProceed = new boolean[2];
+
+    private Runnable forgetPassword;
+    private Runnable dashboardPage;
+
+    public LoginPage(Runnable forgetPassword, Runnable dashboardPage) {
+
+        this.forgetPassword = forgetPassword;
+        this.dashboardPage = dashboardPage;
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = screenSize.width;
@@ -67,7 +81,7 @@ public class LoginPage {
 
         innerPanel = new RoundedPanel(30);
         innerPanel.setBackground(new Color(32, 41, 56));
-        innerPanel.setPreferredSize(new Dimension(400, 430));
+        innerPanel.setPreferredSize(new Dimension(400, length));
         innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
         innerPanel.isFocusable();
         innerPanel.add(Box.createVerticalStrut(40)); // padding at the top
@@ -86,7 +100,6 @@ public class LoginPage {
         emailLabel.setFont(new Font("Roboto", Font.BOLD, 12));
         emailLabel.setForeground(Color.LIGHT_GRAY);
         emailLabel.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0)); // small left padding
-        
 
         emailLabelPanel.add(emailLabel, BorderLayout.WEST);
         innerPanel.add(emailLabelPanel);
@@ -112,6 +125,13 @@ public class LoginPage {
                     emailField.setText("");
                     emailField.setForeground(Color.WHITE);
                 }
+                emailValidLabel.setVisible(false);
+                emailSpacer.setVisible(false);
+                if (isEmailFieldSpaced) {
+                    length -= 10;
+                    innerPanel.setPreferredSize(new Dimension(400, length));
+                    isEmailFieldSpaced = false;
+                }
             }
 
             @Override
@@ -134,7 +154,7 @@ public class LoginPage {
         emailValidLabelPanel.setMaximumSize(new Dimension(320, 20));
 
         emailValidLabel = new JLabel("Please enter a valid email address");
-        emailValidLabel.setFont(new Font("Roboto", Font.BOLD, 12));
+        emailValidLabel.setFont(new Font("Roboto", Font.BOLD, 10));
         emailValidLabel.setForeground(new Color(151, 0, 0));
         emailValidLabel.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0)); // small left padding
         emailValidLabel.setVisible(false);
@@ -170,13 +190,15 @@ public class LoginPage {
         passField.setForeground(Color.WHITE);
         passField.setCaretColor(Color.GRAY);
         passField.setBorder(new RoundedBorder(new Color(50, 60, 80), 10));
-//        passField.setBorder(BorderFactory.createCompoundBorder(
-//                BorderFactory.createLineBorder(new Color(50, 60, 80)),
-//                BorderFactory.createEmptyBorder(10, 10, 10, 10)
-//        ));
         passField.setAlignmentX(Component.CENTER_ALIGNMENT);
         passField.setText(placeholder2);
         passField.setForeground(Color.GRAY);
+
+        JPanel passValidPanel = new JPanel();
+        JLabel passValidLabel = new JLabel("This field is mendatory");
+        Component passValidSpacer = Box.createVerticalStrut(10);
+        validityLabel(passValidPanel, passValidLabel, passValidSpacer);
+
         passField.addFocusListener(new FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
@@ -184,6 +206,15 @@ public class LoginPage {
                 if (passField.getText().equals(placeholder2)) {
                     passField.setText("");
                     passField.setForeground(Color.WHITE);
+                }
+
+                passValidLabel.setVisible(false);
+                passValidSpacer.setVisible(false);
+
+                if (isPassFieldSpaced) {
+                    length -= 10;
+                    innerPanel.setPreferredSize(new Dimension(400, length));
+                    isPassFieldSpaced = false;
                 }
             }
 
@@ -198,7 +229,51 @@ public class LoginPage {
         });
 
         innerPanel.add(passField);
-        innerPanel.add(Box.createVerticalStrut(20)); // space before next component
+        innerPanel.add(passValidSpacer);
+        innerPanel.add(passValidPanel);
+//        innerPanel.add(Box.createVerticalStrut(5)); // space before next component
+//        innerPanel.add(Box.createVerticalStrut(5)); // space before next component
+
+        // Resend label aligned left
+        JPanel resendLabelPanel = new JPanel();
+        resendLabelPanel.setLayout(new BorderLayout());
+        resendLabelPanel.setOpaque(false);
+        resendLabelPanel.setMaximumSize(new Dimension(320, 20));
+//        resendLabelPanel.setFocusable(true);
+
+        JLabel resendLabel = new JLabel("Forgot password");
+        resendLabel.setFont(new Font("Roboto", Font.BOLD, 12));
+        resendLabel.setForeground(new Color(27, 90, 156));
+        resendLabel.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0)); // small left padding
+        resendLabel.setFocusable(true);
+
+        resendLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                resendLabel.setForeground(new Color(79, 70, 229)); // darker indigo
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                resendLabel.setForeground(new Color(27, 90, 156));
+            }
+        });
+
+        resendLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+
+                if (!showForgetPasswordPage) {
+                    showForgetPasswordPage = true;
+                }
+                if (forgetPassword != null) {
+                    forgetPassword.run();
+                }
+            }
+        });
+
+        resendLabelPanel.add(resendLabel, BorderLayout.WEST);
+        innerPanel.add(resendLabelPanel);
+        innerPanel.add(Box.createVerticalStrut(5));
 
         // Login Button
         signInButton = new RoundedButton("Sign in");
@@ -216,7 +291,7 @@ public class LoginPage {
                 signInButton.setBackground(new Color(99, 102, 241));
             }
         });
-        
+
         signInButton.addActionListener(e -> {
             if (emailField.getText().isEmpty() || emailField.getText().equals(placeholder)) {
                 emailValidLabel.setText("This field is mendatory");
@@ -224,9 +299,126 @@ public class LoginPage {
                 emailValidLabel.setVisible(true);
                 emailSpacer.setVisible(true);
 
-            } else {
+                if (!isEmailFieldSpaced) {
+                    length += 10;
+                    isEmailFieldSpaced = true;
+                }
 
+                if (canProceed[0]) {
+                    canProceed[0] = false;
+                }
+
+            } else if (isValidEmail(emailField.getText())) {
+                if (!UserDataHandler.isValidEmail(emailField.getText())) {
+                    emailValidLabel.setText("This email is not registered");
+                    emailField.setBorder(new RoundedBorder(new Color(150, 0, 0), 10));
+                    emailValidLabel.setVisible(true);
+                    emailSpacer.setVisible(true);
+
+                    if (!isEmailFieldSpaced) {
+                        length += 10;
+                        isEmailFieldSpaced = true;
+                    }
+
+                    if (canProceed[0]) {
+                        canProceed[0] = false;
+                    }
+
+                } else {
+                    if (!canProceed[0]) {
+                        canProceed[0] = true;
+                    }
+                }
+            } else if (!UserDataHandler.isValidUsername(emailField.getText())) {
+                emailValidLabel.setText("This username is not registered");
+                emailField.setBorder(new RoundedBorder(new Color(150, 0, 0), 10));
+                emailValidLabel.setVisible(true);
+                emailSpacer.setVisible(true);
+
+                if (!isEmailFieldSpaced) {
+                    length += 10;
+                    isEmailFieldSpaced = true;
+                }
+
+                if (canProceed[0]) {
+                    canProceed[0] = false;
+                }
+            } else {
+                if (!canProceed[0]) {
+                    canProceed[0] = true;
+                }
             }
+
+            if (passField.getText().isEmpty() || passField.getText().equals(placeholder2)) {
+                passValidLabel.setText("This field is mendatory");
+                passField.setBorder(new RoundedBorder(new Color(150, 0, 0), 10));
+                passValidLabel.setVisible(true);
+                passValidSpacer.setVisible(true);
+
+                if (!isPassFieldSpaced) {
+                    length += 10;
+                    isPassFieldSpaced = true;
+                }
+
+                if (canProceed[1]) {
+                    canProceed[1] = false;
+                }
+            } else if (isValidEmail(emailField.getText())) {
+                if (!(UserDataHandler.isPassCorrectByEmail(emailField.getText(), passField.getText()))) {
+                    passValidLabel.setText("The password is incorrect");
+                    passField.setBorder(new RoundedBorder(new Color(150, 0, 0), 10));
+                    passValidLabel.setVisible(true);
+                    passValidSpacer.setVisible(true);
+
+                    if (!isPassFieldSpaced) {
+                        length += 10;
+                        isPassFieldSpaced = true;
+                    }
+
+                    if (canProceed[1]) {
+                        canProceed[1] = false;
+                    }
+                } else {
+                    if (!canProceed[1]) {
+                        canProceed[1] = true;
+                    }
+                }
+            } else if (!(UserDataHandler.isPassCorrectByUser(emailField.getText(), passField.getText()))) {
+                passValidLabel.setText("The password is incorrect");
+                passField.setBorder(new RoundedBorder(new Color(150, 0, 0), 10));
+                passValidLabel.setVisible(true);
+                passValidSpacer.setVisible(true);
+
+                if (!isPassFieldSpaced) {
+                    length += 10;
+                    isPassFieldSpaced = true;
+                }
+
+                if (canProceed[1]) {
+                    canProceed[1] = false;
+                }
+            } else {
+                if (!canProceed[1]) {
+                    canProceed[1] = true;
+                }
+            }
+
+            innerPanel.setPreferredSize(new Dimension(400, length));
+
+            for (boolean proceed : canProceed) {
+                if (!proceed) {
+                    showDashboardPage = false;
+                } else {
+                    showDashboardPage = true;
+                }
+            }
+
+            if (showDashboardPage) {
+                if (dashboardPage != null) {
+                    dashboardPage.run();
+                }
+            }
+
         });
 
         innerPanel.add(Box.createVerticalStrut(20));
@@ -254,6 +446,29 @@ public class LoginPage {
 
     public RoundedButton getSignInButton() {
         return signInButton;
+    }
+
+    private void validityLabel(JPanel panel, JLabel label, Component spacer) {
+        panel.setLayout(new BorderLayout());
+        panel.setOpaque(false);
+        panel.setMaximumSize(new Dimension(320, 12));
+
+        label.setFont(new Font("Roboto", Font.BOLD, 10));
+        label.setForeground(new Color(151, 0, 0));
+        label.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0)); // small left padding
+        label.setVisible(false);
+
+        spacer.setVisible(false);
+
+        panel.add(label, BorderLayout.WEST);
+    }
+
+    public static boolean ShowForgetPasswordPage() {
+        return showForgetPasswordPage;
+    }
+
+    public static boolean ShowDashboardPage() {
+        return showDashboardPage;
     }
 
 }
